c509a7a25fd5d789581584de5c9545904fa9a1ce
Shubham Gupta <78054098+Shubhamgupta171@users.noreply.github.com>
2024-04-26 16:53:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+fetch('https://icanhazdadjoke.com/slack')
+.then(data=>data.json())
+.then(jokedata=>{
+    const joketext=jokedata.attachments[0].text;
+    const jokeelement=document.getElementById('jokeelement');
+    jokeelement.innerText=joketext;
+})
